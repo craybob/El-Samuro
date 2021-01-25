d4986143ea5547e250d5e484e1a0fb678752c0cb
craybo <uncommonalmaz@gmail.com>
2021-01-26 00:26:46 +0600

    first changes )

---
+++ El Samuro/Assets/Scripts/player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player : MonoBehaviour
+{
+    protected joyButton joybutton;
+    public int coins;
+    public Text damageText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        joybutton = FindObjectOfType<joyButton>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (joybutton.pressed)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                damageText.text = "" + i;
+                coins += i;
+            }
+        }
+    }
+}
